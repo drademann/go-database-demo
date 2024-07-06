c26741cf252566600c8262bbb0f4e7727e4dc8e4
Dirk Rademann <dirk.rademann@icloud.com>
2024-07-06 15:41:01 +0200

    now with a transaction

--- sql/main.go
+++ sql/main.go
@@ -22,12 +22,25 @@ func main() {
 	}
 	defer conn.Close(context.Background())
 
+	tx, err := conn.Begin(context.Background())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to start transaction: %v\n", err)
+		os.Exit(1)
+	}
+
 	var id int64
 	var firstName, lastName string
 	err = conn.QueryRow(context.Background(), "select id, first_name, last_name from app_user").Scan(&id, &firstName, &lastName)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "query row failed: %v\n", err)
+		tx.Rollback(context.Background())
 		os.Exit(1)
 	}
 	log.Printf("found user %d: %s %s\n", id, firstName, lastName)
+
+	err = tx.Commit(context.Background())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to commit: %v\n", err)
+		os.Exit(1)
+	}
 }
